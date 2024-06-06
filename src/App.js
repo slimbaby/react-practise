096c627c43c7bb5ca0732ed99efef211980d5d56
slimbaby <460467982@qq.com>
2024-06-06 21:27:59 +0800

    feat: react练习jsx和moment时间格式化

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import './App.css'
-
+import { Component } from 'react'
+import moment from 'moment/moment'
 const name = '我是大美女'
-function App() {
-  return (
-    <ul className="App">
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <div>{name}</div>
-    </ul>
-  )
+class Aaa extends Component {
+  state = {
+    tabs: [
+      { id: 1, name: '热度', type: 'hot' },
+      { id: 2, name: '时间', type: 'time' },
+    ],
+    active: 'hot',
+    list: [
+      {
+        id: 1,
+        author: '周杰伦',
+        comment: '哎哟，不错哦',
+        time: new Date(),
+        attitute: 0,
+      },
+      {
+        id: 2,
+        author: '五月天',
+        comment: '哎哟，不错哦哦',
+        time: new Date(),
+        attitute: 0,
+      },
+      {
+        id: 3,
+        author: '周杰伦',
+        comment: '哎哟，不错哦哦哦',
+        time: new Date(),
+        attitute: -1,
+      },
+    ],
+  }
+  timeFormate(params) {
+    return moment(params).format('YYYY-MM-DD HH:mm:ss')
+  }
+  render() {
+    const { tabs, active, list } = this.state
+    return (
+      <div>
+        {tabs.map((item) => (
+          <div key={item.id} className={item.type === active ? 'active' : ''}>
+            按{item.name}排序
+          </div>
+        ))}
+        {list.map((item) => (
+          <div key={item.id}>按{this.timeFormate(item.time)}排序</div>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App
+export default Aaa

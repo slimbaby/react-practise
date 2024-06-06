a2d69cd30616abb76c6c87a9597096115cccccde
slimbaby <460467982@qq.com>
2024-06-06 12:22:55 +0800

    feat: react练习

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+const name = '我是大美女'
+function App() {
+  return (
+    <ul className="App">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <div>{name}</div>
+    </ul>
+  )
+}
+
+export default App

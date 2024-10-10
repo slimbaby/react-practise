2a08de881d8a691b4d8f9c64955f85d79b43df10
slimbaby <460467982@qq.com>
2024-10-10 17:32:14 +0800

    feat: route练习

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement, decrementAsync } from './store/actions/counter'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { increment, decrement, decrementAsync } from './store/actions/counter'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+const First = () => {
+  return <p>这是第一个页面的内容</p>
+}
+const Second = () => {
+  return <p>这是第二个页面的内容</p>
+}
 const App = () => {
-  const count = useSelector((state) => state.count)
-  const dispatch = useDispatch()
+  // const count = useSelector((state) => state.count)
+  // const dispatch = useDispatch()
   return (
-    <div>
-      <h1>计数器：{count}</h1>
-      <button onClick={() => dispatch(increment(1))}>数量增加</button>
-      <button onClick={() => dispatch(decrement(2))}>数量减少</button>
-      <button onClick={() => dispatch(decrementAsync(2))}>数量异步减少</button>
-    </div>
+    <Router>
+      <div>
+        <h1>路由的基本使用</h1>
+        <ul>
+          <li>
+            <Link to="/first">第一个页面</Link>
+            <input type="tel" />
+          </li>
+          <li>
+            <Link to="/second">第二个页面</Link>
+            <input type="tel" />
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/first" Component={First}></Route>
+          <Route path="/second" Component={Second}></Route>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- src/children.js
+++ src/children.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 const Aaa = ({ children }) => {
   return <div>{children}</div>
